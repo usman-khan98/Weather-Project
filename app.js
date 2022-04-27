3cafc3610527da8c05d0d4c1277dac85b9c94643
usman <usman544k@gmail.com>
2022-04-27 21:46:02 +0500

    added procfile, changed ports, added gitignore

--- app.js
+++ app.js
@@ -37,6 +37,6 @@ app.post("/", function(req, res){
 
 
 
-app.listen(3000, function(){
-  console.log("Server Started at port:3000");
-})
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server running on port:3000 and Heroku");
+});
